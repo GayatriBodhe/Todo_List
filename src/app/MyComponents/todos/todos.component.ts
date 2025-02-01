ce79c29f0ccb4af6075cf0935ae436afa0a8943e
GayatriBodhe <gayatrimbodhe12@gmail.com>
2025-02-01 12:30:34 +0530

    init

---
+++ src/app/MyComponents/todos/todos.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Todo } from '../../Todo';
+
+@Component({
+  selector: 'app-todos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todos.component.html',
+  styleUrl: './todos.component.css'
+})
+export class TodosComponent implements OnInit {
+  Todos: Todo[];
+  constructor() { 
+    this.Todos = [
+      {
+        sno: 1,
+        title: "This is title 1",
+        desc: "This is description 1",
+        active: true
+      },
+      {
+        sno: 2,
+        title: "This is title 2",
+        desc: "This is description 2",
+        active: true
+      },
+    ]
+  }
+
+  ngOnInit(): void {
+  }
+
+}
